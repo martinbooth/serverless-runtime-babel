d4132aa22965a025f0c2cbe26de89fc3da631f23
Egor Kislitsyn <egor@kislitsyn.com>
2016-03-21 16:46:17 +0700

    adds promise support

--- templates/handler.js
+++ templates/handler.js
@@ -1,7 +1,7 @@
-
 export default (event, context) => {
-  return context.done(null, {
+  // must return a promise, a JSON.stringify compatible data, null or nothing.
+  return {
     message: 'Go Serverless! Your Lambda function executed successfully!'
-  });
-};
+  }
+}
 
